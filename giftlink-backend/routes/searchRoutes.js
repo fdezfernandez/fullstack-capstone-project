4a88e9738cb5c01d91669b6f8ec23f46b932afb5
fdezfernandez <fdez.fernandez.miguel@gmail.com>
2026-06-29 10:17:27 -0400

    Complete Search API service lab

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -5,7 +5,7 @@ const connectToDatabase = require('../models/db');
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
+        // Task 1
         const db = await connectToDatabase();
 
         const collection = db.collection("gifts");
@@ -13,12 +13,12 @@ router.get('/', async (req, res, next) => {
         // Initialize the query object
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        if (req.query.name) {
+        // Task 2
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" };
         }
 
-        // Task 3: Add other filters to the query
+        // Task 3
         if (req.query.category) {
             query.category = req.query.category;
         }
@@ -31,7 +31,7 @@ router.get('/', async (req, res, next) => {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
+        // Task 4
         const gifts = await collection.find(query).toArray();
 
         res.json(gifts);
