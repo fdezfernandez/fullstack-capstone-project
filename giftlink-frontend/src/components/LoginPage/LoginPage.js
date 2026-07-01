998631e0717a3d6b824696e9edda556359de6a2d
fdezfernandez <fdez.fernandez.miguel@gmail.com>
2026-07-01 06:37:20 -0400

    Implemented backend login integration

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,12 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
 
-    const handleLogin = async () => {
-        console.log("Inside handleLogin");
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app');
+        }
+    }, [navigate]);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        });
+
+        const json = await res.json();
+        console.log('Json', json);
+
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', json.userName);
+            sessionStorage.setItem('email', json.userEmail);
+
+            setIsLoggedIn(true);
+            navigate('/app');
+        } else {
+            document.getElementById("email").value = "";
+            document.getElementById("password").value = "";
+
+            setIncorrect("Wrong password. Try again.");
+
+            setTimeout(() => {
+                setIncorrect("");
+            }, 2000);
+        }
     };
 
     return (
@@ -24,7 +72,10 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                    setIncorrect("");
+                                }}
                             />
                         </div>
 
@@ -36,8 +87,23 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                    setIncorrect("");
+                                }}
                             />
+
+                            <span
+                                style={{
+                                    color: 'red',
+                                    height: '.5cm',
+                                    display: 'block',
+                                    fontStyle: 'italic',
+                                    fontSize: '12px'
+                                }}
+                            >
+                                {incorrect}
+                            </span>
                         </div>
 
                         <button
